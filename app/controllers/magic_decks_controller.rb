5667bab0aadb6cd601bdf26a61bcce6765c63cbc
so1itaryrove <so1itaryrove@gmail.com>
2022-05-14 13:45:50 +0900

    add playablitiy

--- app/controllers/magic_decks_controller.rb
+++ app/controllers/magic_decks_controller.rb
@@ -10,6 +10,7 @@ class MagicDecksController < ApplicationController
       if @magic_deck  
         chart = simulate(@magic_deck.cards)
         @played_over_drawed = chart[:played_over_drawed]
+        @can_played_over_drawed = chart[:can_played_over_drawed]
         @played_over_sims = chart[:played_over_sims]
         @mana_curves = chart[:mana_curves]
         @magic_deck.delete
@@ -69,16 +70,19 @@ class MagicDecksController < ApplicationController
 
       played_over_drawed = {}
       played_over_sims = {}
+      can_played_over_drawed = {}
       sorted.map do |card| 
         spell = card.contents.select {|c| c.types != "Land"}.first
         if spell
           mana_cost = spell.mana_cost
           name = "#{card.name.split('//')[0].chomp.strip} : #{mana_cost}"
-          played, drawed = card.count
+          played, drawed, can_played = card.count
           a = played.to_f / config.simulations * 100
           b = played.to_f / drawed * 100
+          c = can_played.to_f / drawed * 100
           played_over_sims[name] = a.round(1)
           played_over_drawed[name] = b.round(1)
+          can_played_over_drawed[name] = c.round(1)
         end
       end
 
@@ -108,6 +112,7 @@ class MagicDecksController < ApplicationController
       {
         played_over_drawed: played_over_drawed, 
         played_over_sims: played_over_sims, 
+        can_played_over_drawed: can_played_over_drawed, 
         mana_curves: mana_curves
       }
     end
