5e236db36a2b89fd98aa3ce7eb3949c2d27ced5c
so1itaryrove <so1itaryrove@gmail.com>
2022-05-19 21:06:48 +0900

    remove usage contact

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   root to: 'magic_decks#index'
   get 'magic_decks', to: "magic_decks#index"
   post 'magic_decks', to: "magic_decks#create"
-  get 'usage', to: "magic_decks#usage"
-  get 'contact', to: "magic_decks#contact"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
