4bf51a49eed51273cfa0b30a746d5c1292a5be75
so1itaryrove <so1itaryrove@gmail.com>
2022-05-20 19:24:49 +0900

    multi lung

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ Rails.application.routes.draw do
   get 'magic_decks', to: "magic_decks#index"
   post 'magic_decks', to: "magic_decks#create"
 
+  scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
+    # For details on the DSL available within this file, see 
+    get '/', to: "magic_decks#index"
+    get 'magic_decks', to: "magic_decks#index"
+    post 'magic_decks', to: "magic_decks#create"
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
